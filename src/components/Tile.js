b6fcaaf35dbbe30e5c138dd610f2e5087a4205a4
mohit <mohitd2288@gmail.com>
2023-03-21 23:52:46 +0530

    added review

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,15 +1,21 @@
 function Tile(props) {
-    const { image, title, subtitle } = props
+    const { image, title, subtitle, top } = props
     return (
-        <div className="flex h-96 flex-col justify-center pt-1">
+        <div
+            className={`flex flex-col justify-center pt-1 ${
+                top ? 'h-96' : 'h-80 sm:h-[26rem] md:h-[30rem] lg:h-72'
+            }`}
+        >
             <div
                 style={{ backgroundImage: `url(${image})` }}
-                className="relative h-full flex-auto bg-cover bg-center bg-no-repeat text-4xl text-white"
+                className="bg relative flex-auto bg-cover bg-no-repeat text-4xl text-white"
             >
                 <div className="h-full w-full bg-black opacity-10 transition-all ease-in hover:opacity-5"></div>
-                <div className="absolute top-1/2 flex w-full justify-center font-bold">
-                    {title}
-                </div>
+                {top && (
+                    <div className="absolute top-1/2 flex w-full justify-center font-bold">
+                        {title}
+                    </div>
+                )}
             </div>
             <div className="py-1 text-sm sm:flex sm:justify-between">
                 <h1>{subtitle}</h1>
---
+++ src/libs/reviewData.js
@@ -0,0 +1,22 @@
+const reviewData = [
+    {
+        key: 41,
+        image: 'https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1669265478/amjurvk96rj3nhifaezo.png',
+        title: '10 Cool Finds',
+        subtitle: 'Our roundup of standout projects',
+    },
+    {
+        key: 42,
+        image: 'https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1669265541/dgmmd7rpcsipmzhzteva.png',
+        title: 'Team Favourites',
+        subtitle: 'Respiray & more team favourites',
+    },
+    {
+        key: 23,
+        image: 'https://c1.iggcdn.com/indiegogo-media-prod-cld/image/upload/f_auto/v1673506121/p9nkhezs3dkagkz4isor.png',
+        title: 'Spend Under 100',
+        subtitle: 'Check out these innovative steals',
+    },
+]
+
+export default reviewData
