018268140f766838a27f3afe0a194a8c742f2c81
mohit <mohitd2288@gmail.com>
2023-03-24 22:25:13 +0530

    added footer and changed fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
 import '@/styles/globals.css'
-import { Rubik } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const rubik = Rubik({ weight: ['300'], subsets: ['latin'] })
+const poppins = Poppins({ weight: ['300'], subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
     return (
-        <main className={rubik.className}>
+        <main className={poppins.className}>
             <Component {...pageProps} />
         </main>
     )
